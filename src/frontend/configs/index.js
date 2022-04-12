2d2b36b6745c9a012d92b483dee8da02561c80af
nhanlnt-96 <nhanlnt@hotmail.com>
2022-04-13 00:24:37 +0700

    project-feature: complete create project feature

--- src/frontend/configs/index.js
+++ src/frontend/configs/index.js
@@ -2,3 +2,5 @@ export * from "./headerMenu";
 export * from "./cardData";
 export * from "./topSellerData";
 export * from "./todayPickData";
+export * from "./popularCollectionData";
+export * from "./projectFeatureData";
